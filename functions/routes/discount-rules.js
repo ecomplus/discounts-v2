78bb442551cadbe9175fd814641c52b83a0c6667
Wisley Alves <wisley7lgithub@gmail.com>
2024-07-24 11:38:19 -0300

    fix(discount-rules): fixes discount rules validation

--- functions/routes/discount-rules.js
+++ functions/routes/discount-rules.js
@@ -1,33 +1,13 @@
 const getAppData = require('../lib/store-api/get-app-data')
+const { validateDateRange } = require('../lib/helpers')
 
-const validateDateRange = rule => {
-  // filter campaings by date
-  const timestamp = Date.now()
-  if (rule.date_range) {
-    if (rule.date_range.start && new Date(rule.date_range.start).getTime() > timestamp) {
-      return false
-    }
-    if (rule.date_range.end && new Date(rule.date_range.end).getTime() < timestamp) {
-      return false
-    }
-  }
-  return true
-}
-
-const checkCampaign = (rules, domain) => {
-  let i = 0
-  while (i < rules?.length) {
-    const rule = rules[i]
-    if (
-      (!rule.domain || !new RegExp(`^(https?://)?${rule.domain}$`).test(domain) || !validateDateRange(rule)) ||
-      (!rule.product_ids?.length && !rule.category_ids?.length && !rule.excluded_product_ids?.length) ||
-      ((rule.utm_campaign || (rule.customer_ids && rule.customer_ids.length) || rule.discount_coupon))
-    ) {
-      rules.splice(i, 1)
-    } else {
-      i++
-    }
-  }
+const isValidRule = (rule, domain) => {
+  const isInvalidRule = Boolean(
+    !validateDateRange(rule) || (rule.domain && rule.domain !== domain) ||
+    (!rule.product_ids?.length && !rule.category_ids?.length && !rule.excluded_product_ids?.length) ||
+    ((rule.utm_campaign || (rule.customer_ids && rule.customer_ids.length) || rule.discount_coupon))
+  )
+  return !isInvalidRule
 }
 
 exports.get = async ({ appSdk }, req, res) => {
@@ -43,30 +23,22 @@ exports.get = async ({ appSdk }, req, res) => {
       .send({ message: 'Parameter "domain" not found' })
   }
 
+  domain = domain.replace(/https?:\/\/|:\d+/g, '')
+
   if (storeId > 100) {
     const appData = await appSdk.getAuth(storeId)
       .then(auth => getAppData({ appSdk, storeId, auth }))
       .catch(console.error)
 
-    let discountRules
-    let productKitDiscounts
-    let freebiesRules
-
-    if (appData) {
-      discountRules = appData.discount_rules
-      productKitDiscounts = appData.product_kit_discounts
-      freebiesRules = appData.freebies_rules
-
-      checkCampaign(discountRules, domain)
-      checkCampaign(productKitDiscounts, domain)
-      checkCampaign(freebiesRules, domain)
-    }
+    const discountRules = appData?.discount_rules || []
+    const productKitDiscounts = appData?.product_kit_discounts || []
+    const freebiesRules = appData?.freebies_rules || []
 
     return res.send({
       domain,
-      discount_rules: discountRules || [],
-      product_kit_discounts: productKitDiscounts || [],
-      freebies_rules: freebiesRules || []
+      discount_rules: discountRules.filter((rule) => isValidRule(rule, domain)),
+      product_kit_discounts: productKitDiscounts.filter((rule) => isValidRule(rule, domain)),
+      freebies_rules: freebiesRules.filter((rule) => isValidRule(rule, domain))
     })
   }
 
